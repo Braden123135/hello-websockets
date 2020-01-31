568ba5d308c8b7a5f08110a8e7fac12e24c4f681
Braden Estenson <Bradenestenson@live.com>
2020-01-30 19:10:59 -0600

    made cool web socket

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const http = require('http');
+const WebSocket = require('ws');
 
 const app = express();
 app.use(express.static('public'));
 
 const server = http.createServer(app);
+const wss = new WebSocket.Server({server:server});
+wss.on('connection',function(wss,request){
+    console.log('Connected');
+});
 
 server.listen(8080,function(){
     console.log('App listening on poert 8080');
